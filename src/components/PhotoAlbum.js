52947a7757368affa472111a10db4ea7b3ee7aa0
l33th <l33th@protonmail.ch>
2021-10-17 08:21:21 +0200

    Secondary Button

--- src/components/PhotoAlbum.js
+++ src/components/PhotoAlbum.js
@@ -14,7 +14,7 @@ import {
 
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const PhotoAlbum = () => {
 	return (
@@ -26,44 +26,55 @@ const PhotoAlbum = () => {
 					<Typography variant='h6'>Photo Album</Typography>
 				</Toolbar>
 			</AppBar>
-			<main>
-				<div>
-					<Container maxWidth='sm'>
-						<Typography
-							variant='h2'
-							align='center'
-							color='textPrimary'
-							gutterBottom
-						>
-							Photo Album
-						</Typography>
-						<Typography
-							varian='h5'
-							align='center'
-							color='textSecondary'
-							paragraph
-						>
-							<strong>
-								Lorem ipsum dolor sit amet consectetur,
-								adipisicing elit. Dolor omnis deleniti minima
-								saepe blanditiis tempora, dicta error mollitia
-								quas quaerat voluptatum eaque consequatur
-								distinctio suscipit assumenda velit tempore?
-								Excepturi, cum.
-							</strong>
-						</Typography>
-						<div>
-							<Grid container spacing={2} justify='center'>
-								<Grid item>
-									<Button variant='contained' color='primary'>
-										See my photos
-									</Button>
+			<div>
+				<main>
+					<div>
+						<Container maxWidth='sm'>
+							<Typography
+								variant='h2'
+								align='center'
+								color='textPrimary'
+								gutterBottom
+							>
+								Photo Album
+							</Typography>
+							<Typography
+								varian='h5'
+								align='center'
+								color='textSecondary'
+								paragraph
+							>
+								<strong>
+									Lorem ipsum dolor sit amet consectetur,
+									adipisicing elit. Dolor omnis deleniti
+									minima saepe blanditiis tempora, dicta error
+									mollitia quas quaerat voluptatum eaque
+									consequatur distinctio suscipit assumenda
+									velit tempore? Excepturi, cum.
+								</strong>
+							</Typography>
+							<div>
+								<Grid container spacing={2} justify='center'>
+									<Grid item>
+										<Button
+											variant='contained'
+											color='primary'
+										>
+											See my photos
+										</Button>
+										<Button
+											variant='outlined'
+											color='primary'
+										>
+											Secondary action
+										</Button>
+									</Grid>
 								</Grid>
-							</Grid>
-						</div>
-					</Container>
-				</div>
-			</main>
+							</div>
+						</Container>
+					</div>
+				</main>
+			</div>
 		</>
 	);
 };
